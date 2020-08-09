70c96554fc94742def03b1e3f0c90afb066a6337
Anonymous <>
2020-08-09 22:27:27 +0800

    remove mistakenly added headers

--- Testcase4-Application-breakdown/online-compiling/test_vectors/hello/build-aux/snippet/_Noreturn.h
+++ Testcase4-Application-breakdown/online-compiling/test_vectors/hello/build-aux/snippet/_Noreturn.h
@@ -1,14 +1,3 @@
-/* * Copyright (c) 2020 Institution of Parallel and Distributed System, Shanghai Jiao Tong University
- * ServerlessBench is licensed under the Mulan PSL v1.
- * You can use this software according to the terms and conditions of the Mulan PSL v1.
- * You may obtain a copy of Mulan PSL v1 at:
- *     http://license.coscl.org.cn/MulanPSL
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v1 for more details.
- */
-
 #if !defined _Noreturn && __STDC_VERSION__ < 201112
 # if (3 <= __GNUC__ || (__GNUC__ == 2 && 8 <= __GNUC_MINOR__) \
       || 0x5110 <= __SUNPRO_C)
--- Testcase4-Application-breakdown/online-compiling/test_vectors/mosh/src/tests/base64_vector.h
+++ Testcase4-Application-breakdown/online-compiling/test_vectors/mosh/src/tests/base64_vector.h
@@ -1,14 +1,3 @@
-/* * Copyright (c) 2020 Institution of Parallel and Distributed System, Shanghai Jiao Tong University
- * ServerlessBench is licensed under the Mulan PSL v1.
- * You can use this software according to the terms and conditions of the Mulan PSL v1.
- * You may obtain a copy of Mulan PSL v1 at:
- *     http://license.coscl.org.cn/MulanPSL
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v1 for more details.
- */
-
 struct base64_test_row { const unsigned char native[17]; const char encoded[25]; };
 typedef base64_test_row *base64_test_vector;
 extern base64_test_row static_base64_vector[];
